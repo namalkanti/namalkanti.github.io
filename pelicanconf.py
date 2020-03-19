13196a062dc6bf4f9cbadb7550dbb41e02706916
Niraj Amalkanti <neji49@gmail.com>
2020-03-19 02:03:22 -0400

    Realized publishconf needs to be used instead of pelican conf

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Niraj Amalkanti'
 SITENAME = 'Mitigating Failure'
-SITEURL = 'https://namalkanti.github.io'
+SITEURL = ''
 
 PATH = 'content'
 ARTICLE_PATHS = ['articles']
