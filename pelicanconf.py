36e3ea73d2387be5a8262d3f81ff6a5bed7a6a0a
Niraj Amalkanti <neji49@gmail.com>
2020-03-19 01:43:09 -0400

    Updating pelian conf with https url

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Niraj Amalkanti'
 SITENAME = 'Mitigating Failure'
-SITEURL = ''
+SITEURL = 'https://namalkanti.github.io'
 
 PATH = 'content'
 ARTICLE_PATHS = ['articles']
